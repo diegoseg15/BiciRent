bcf1db3c89e31cb793cb56ee1abbaba8dca0e9dc
Cynthia <cynten@alumni.uv.es>
2024-12-01 17:30:58 +0100

    Conexion JSON, gitignore

--- frontend/src/app/compartido/baseurl.ts
+++ frontend/src/app/compartido/baseurl.ts
@@ -1,2 +1,2 @@
 export const baseURL = 'http://localhost:3000/';
-export const baseAPIURL = 'http://localhost:8080/plc-pls-mps-tutorial/api/'; // <1>
+export const baseAPIURL = 'http://localhost:8080/twcam-pls-plcv-mdps-spa/api/'; // <1>
