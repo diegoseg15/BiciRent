9f669f58d0039ccdc5e083a9044ecea412b9f1d3
diegoseg15 <diegofersv@gmail.com>
2025-01-03 13:03:32 +0100

    conección con el backend del login

--- frontend/src/app/services/usuario.service.ts
+++ frontend/src/app/services/usuario.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Login } from '../compartido/login';
+import { Observable } from 'rxjs';
+import { baseURL } from '../compartido/baseurl'; 
+import {HttpHeaders} from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+const httpOptions= {
+  headers: new HttpHeaders({
+  'Content-Type': 'application/x-www-form-urlencoded',
+  // 'Authorization': 'my-auth-token'
+  })
+  };
+
+  @Injectable({
+    providedIn: 'root'
+  })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  setLogin(user:Login): Observable<Login>{
+    console.log(user);
+    
+    return this.http.post<Login>(baseURL + 'api/login', "correo="+user.correo+"&password="+user.password, httpOptions);
+  }
 }
