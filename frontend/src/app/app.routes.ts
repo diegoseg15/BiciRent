0fb9a37c0518c335f37c81e60c8a101457086979
diegoseg15 <diegofersv@gmail.com>
2025-01-03 13:04:50 +0100

    conección del componente chooseBike

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ChooseBikeComponent } from './choose-bike/choose-bike.component';
 
+//iniciado sesión
 export const routes: Routes = [
-    {path:'', component:LoginComponent},
+    {path:'', component:ChooseBikeComponent},
     {path:'login', component:LoginComponent},
     {path:'signup', component:RegisterComponent}
 ];
+
+//sin iniciar sesión
+/**
+ export const routes: Routes = [
+    {path:'', component:homeComponent},
+];
+ */
---
+++ frontend/src/app/choose-bike/choose-bike.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-bike',
+  standalone: true,
+  imports: [],
+  templateUrl: './choose-bike.component.html',
+  styleUrl: './choose-bike.component.css'
+})
+export class ChooseBikeComponent {
+
+}
